6fac805f204f4b52d3bb0a3d5bd9940c6ae5fbf2
thomas heckmann <thomas.kolbeck@gmail.com>
2017-10-03 13:04:57 +0200

    added search_by_titles from ZXDB to suggestions

--- create-title-suggestions.js
+++ create-title-suggestions.js
@@ -1,28 +1,24 @@
 /**
 
-SELECT
-   e.id,
-   aka.title,
-   e.title 
-FROM
-   aliases aka 
-   LEFT JOIN
-      entries e 
-      on e.id = aka.entry_id 
-order by
-   e.id
-
+id = 0002259 (suggestions based on fulltitle)
+id = 0002259-aNNNN (suggestions based on alias)
+id = 0002259-sNNNN (suggestions based on search_by_titles in ZXDB)
 */
 
 var es = require('./esConfig');
 var db = require('./dbConfig');
+var _ = require('lodash');
 
 var json_output_dir = 'data/processed/json/';
 var allcombinations = require('allcombinations')
 
 var jsonfile = require('jsonfile')
 
-function createSuggestions(id, outputtitle, title) {
+/**
+ id = id in index, see above
+ outputId = zxdb_id for this item
+ */
+function createSuggestions(id, outputId, outputtitle, title) {
     // split title by space, comma, dash, colon, semi-colon
     var titlewords = title.toLowerCase().split(/[\: ,-]+/);
     // if more than 5 words, keep first 5 only (to limit combinations)
@@ -47,7 +43,7 @@ function createSuggestions(id, outputtitle, title) {
         }
     });
 
-    console.log(id + " (" + outputtitle + ") " + title + " => " + titlewordsCleaned);
+    console.log(outputId + " (" + outputtitle + ") " + title + " => " + titlewordsCleaned);
     var cs = Array.from(allcombinations(titlewordsCleaned));
 
     var input = [title];
@@ -59,13 +55,15 @@ function createSuggestions(id, outputtitle, title) {
         input.push(inputLine.trim());
     });
 
+    input = _.uniqWith(input, _.isEqual);
+
     var item = {
         title: outputtitle,
         suggest: {
             input: input,
             output: outputtitle,
             payload: {
-                title_id: id
+                title_id: outputId
             },
             weight: 10
         }
@@ -102,7 +100,7 @@ var createSuggestionsForFulltitle = function() {
             var id = ('0000000' + results[i].id).slice(-7);
             var fulltitle = results[i].title;
 
-            createSuggestions(id, fulltitle, fulltitle);
+            createSuggestions(id, id, fulltitle, fulltitle);
         }
         done = true;
     });
@@ -123,10 +121,11 @@ var creageSuggestionsForAlsoKnownAs = function() {
         }
         var i = 0;
         for (; i < results.length; i++) {
-            var id = ('0000000' + results[i].id).slice(-7) + '-' + i;
+            var id = ('0000000' + results[i].id).slice(-7) + '-a' + i;
+            var targetId = ('0000000' + results[i].id).slice(-7);
             var fulltitle = results[i].title;
 
-            createSuggestions(id, fulltitle, results[i].alsoknownas);
+            createSuggestions(id, targetId, fulltitle, results[i].alsoknownas);
         }
         done = true;
     });
@@ -137,6 +136,32 @@ var creageSuggestionsForAlsoKnownAs = function() {
     db.closeConnection(connection);
 }
 
-createSuggestionsForFulltitle();
-creageSuggestionsForAlsoKnownAs();
+var creageSuggestionsForZXDBSearchByTitles = function() {
+    console.log('CREATE SUGGESTIONS FOR ALSO KNOWN AS');
+    var connection = db.getConnection();
+    var done = false;
+    // connection.query('select id, title from entries where 1 order by id asc', function(error, results, fields) {
+    connection.query('SELECT DISTINCT e.id, aka.entry_title AS alsoknownas, e.title AS title FROM search_by_titles aka LEFT JOIN entries e ON e.id = aka.entry_id ORDER BY e.id ASC', function(error, results, fields) {
+        if (error) {
+            throw new Error("Can't connect: ", error.stack);
+        }
+        var i = 0;
+        for (; i < results.length; i++) {
+            var id = ('0000000' + results[i].id).slice(-7) + '-s' + i;
+            var targetId = ('0000000' + results[i].id).slice(-7);
+            var fulltitle = results[i].title;
 
+            createSuggestions(id, targetId, fulltitle, results[i].alsoknownas);
+        }
+        done = true;
+    });
+    require('deasync').loopWhile(function() {
+        return !done;
+    });
+    console.log("Finished!");
+    db.closeConnection(connection);
+}
+
+// createSuggestionsForFulltitle();
+// creageSuggestionsForAlsoKnownAs();
+creageSuggestionsForZXDBSearchByTitles();
