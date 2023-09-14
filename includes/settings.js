b9b982efe41d7219016e98579e19e7e133470438
thomasheckmann <thomas.kolbeck@gmail.com>
2023-09-14 21:17:12 +0200

    small fixes

--- includes/settings.js
+++ includes/settings.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const prefix_entries_path = "data/entries/";
+const prefix_entries_path = "data/entries";
 const prefix_magazines_path = "data/magazines/";
 const prefix_screens_path = "data/screens/";
 
